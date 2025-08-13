362adfbd688bedc9dbfba45ab1bd443c6de9e5f1
Diego Oliveira <diegoalves.div@gmail.com>
2025-08-13 00:23:47 -0300

    fix: pequeno erro no ViaCep

--- src/main/java/br/com/emakers/biblioteca_api/data/dto/response/ViaCepResponseDTO.java
+++ src/main/java/br/com/emakers/biblioteca_api/data/dto/response/ViaCepResponseDTO.java
@@ -1,20 +1,16 @@
 package br.com.emakers.biblioteca_api.data.dto.response;
 
+// DTO da resposta ViaCEP utilizado para preencher endereço em Pessoa
 public class ViaCepResponseDTO {
     private String cep;
     private String logradouro;
     private String bairro;
-    private String localidade;
-    private String uf;
+    private String localidade; // cidade
+    private String uf;         // estado
 
     public String getCep() { return cep; }
-    public void setCep(String cep) { this.cep = cep; }
     public String getLogradouro() { return logradouro; }
-    public void setLogradouro(String logradouro) { this.logradouro = logradouro; }
     public String getBairro() { return bairro; }
-    public void setBairro(String bairro) { this.bairro = bairro; }
     public String getLocalidade() { return localidade; }
-    public void setLocalidade(String localidade) { this.localidade = localidade; }
     public String getUf() { return uf; }
-    public void setUf(String uf) { this.uf = uf; }
 }
